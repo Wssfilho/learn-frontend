2c959070575ef02466c633aca4b5d855fceceafc
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-21 21:16:44 -0300

    Refatorar componente Formulario para melhorar a estrutura e o estilo

--- src/app/components/formComponents.tsx
+++ src/app/components/formComponents.tsx
@@ -1,19 +1,19 @@
 "use client";
 export default function Formulario(){
     return(
-        //quero fazer uma caixa com essa div
-        <div className="flex justify-center items-center min-h-screen bg-linear-to-r from-purple-500 via-pink-500 to-red-500">
-            <form action="" className="flex flex-col p-4 text-black bg-white/10 rounded-2xl shadow-md">
-                <p className="mb-2 text-center font-bold text-white">Entre com seu email</p>
-                <input type="email" name="email" className="mb-2 px-3 py-2 border border-x-black rounded-2xl" />
-                <p className="mb-2 text-center font-bold text-white">Entre com sua senha</p>
-                <input type="email" name="email" className="mb-2 px-3 py-2 border border-x-black rounded-2xl" />
-                <button type="submit" onClick={() => alert("Formulário enviado!")} className="px-4 py-2 border border-x-black rounded-2xl bg-gray-100 hover:bg-gray-200">Submit</button>
+        <div className="flex justify-center items-center h-screen w-screen">
+            <form
+                action=""
+                className="flex flex-col p-8 w-96 h-70 space-y-4 text-black bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 rounded-2xl shadow-md"
+            >
+                <p className="mb-0 text-center font-bold text-white">Entre com seu email</p>
+                <input type="email" name="email" className="px-3 py-2 border border-black rounded-2xl" />
+                <p className="mb-0 text-center font-bold text-white">Entre com sua senha</p>
+                <input type="password" name="password" className="px-3 py-2 border border-black rounded-2xl" />
+                <button type="submit" onClick={() => alert("Formulário enviado!")} className="px-4 py-2 border border-black rounded-2xl bg-gray-100 hover:bg-gray-200">
+                    Submit
+                </button>
             </form>
         </div>
-
-
-
     );
-    
 }
