87f2c2db8888a80842292a00447a9bb9e32b8dfe
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-22 18:11:23 -0300

    Refatorar componente Formulario para melhorar a estrutura e a usabilidade

--- src/app/components/formComponents.tsx
+++ src/app/components/formComponents.tsx
@@ -1,19 +1,28 @@
 "use client";
+
+import { useRouter } from "next/navigation";
+import Header from "./header";
+
 export default function Formulario(){
+    const router = useRouter();
     return(
+        <>
         <div className="flex justify-center items-center h-screen w-screen">
             <form
                 action=""
-                className="flex flex-col p-8 w-96 h-70 space-y-4 text-black bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 rounded-2xl shadow-md"
+                className="flex flex-col p-8 w-96 h-auto space-y-4 text-black bg-gradient-to-r rounded-2xl shadow-xl"
             >
-                <p className="mb-0 text-center font-bold text-white">Entre com seu email</p>
-                <input type="email" name="email" className="px-3 py-2 border border-black rounded-2xl" />
-                <p className="mb-0 text-center font-bold text-white">Entre com sua senha</p>
-                <input type="password" name="password" className="px-3 py-2 border border-black rounded-2xl" />
-                <button type="submit" onClick={() => alert("Formulário enviado!")} className="px-4 py-2 border border-black rounded-2xl bg-gray-100 hover:bg-gray-200">
+                
+                <img src="/img/login.svg" className="w-20 h-20 mx-auto mb-4" alt="Login" />
+                <p className="flex justify-center items-center ">Bem vindo!</p>
+                <input placeholder="Email" type="email" name="email" className="px-3 py-2 border border-black rounded-2xl" />
+                <input placeholder="Senha" type="password" name="password" className="px-3 py-2 border border-black rounded-2xl" />
+                <button type="button" onClick={() => router.push('/home')} className="px-4 py-2 border border-black rounded-2xl bg-gray-100 hover:bg-gray-200">
+
                     Submit
                 </button>
             </form>
         </div>
+        </>
     );
 }
