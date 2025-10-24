09f3172390efe60016d628c82474c101685cfc36
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-10-24 15:05:54 -0300

    Adicionar lógica de manipulação de formulário no modal de adicionar membro

--- src/app/home/modalAddMembro.tsx
+++ src/app/home/modalAddMembro.tsx
@@ -6,10 +6,25 @@ interface ModalAddMembroProps {
 }
 
 export default function ModalAddMembro({ isOpen, onClose }: ModalAddMembroProps) {
+
+
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target as HTMLFormElement);
+        const nomeCivil = formData.get("nomeCivil");
+        const nomeHeroi = formData.get("nomeHeroi");
+        const idade = formData.get("idade");
+        const time = formData.get("time");
+        const pais = formData.get("pais");
+        // Lógica para adicionar o membro
+    }
+
+
     return(
         <>
             {isOpen && (
-                <div className="fixed inset-0 bg-opacity-40 bg-black  flex items-center justify-center z-50">
+                <div className="fixed inset-0  bg-black  flex items-center justify-center z-50">
                     <div className="font-sans bg-white rounded-3xl p-8 w-11/12 max-w-md shadow-2xl relative">
                         <h2 className="text-xl mb-4 font-bold">Adicionar Membro</h2>
                         <form>
@@ -35,7 +50,7 @@ export default function ModalAddMembro({ isOpen, onClose }: ModalAddMembroProps)
                             </div>  
                             <div className="flex justify-end gap-2">
                                 <button type="button" className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600" onClick={onClose}>Cancelar</button>
-                                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Adicionar</button>   
+                                <button type="submit" onClick={handleSubmit} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Adicionar</button>
                             </div>
                         </form>
                     </div>
